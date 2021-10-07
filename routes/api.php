ad4f8be6510a91bd790284afa3e6eebfe7334f41
cliff mathebula <climathe1@gmail.com>
2021-10-07 03:57:47 -0700

    feat: added route to get fish by aquarium id

--- routes/api.php
+++ routes/api.php
@@ -29,5 +29,6 @@ Route::group([
     Route::post('/aquariums', [AquariumsController::class, 'create'])->name('aquariums.create'); 
     Route::post('/fish', [FishController::class, 'create'])->name('fish.create');    
     Route::get('/fish', [FishController::class, 'show_all'])->name('fish.show_all'); 
-    Route::get('/get_fish', [FishController::class, 'find_fish']); 
+    Route::get('/get_fish', [FishController::class, 'find_fish']);
+    Route::get('/aquafish', [FishController::class, 'findByAquarium']); 
 });
